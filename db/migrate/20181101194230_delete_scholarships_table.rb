46692c26177cdd37ad9817d1f1c7976610d38a60
Becina Ganther <becina_ganther@college.harvard.edu>
2018-12-04 20:00:14 -0500

    unsure migrations

--- db/migrate/20181101194230_delete_scholarships_table.rb
+++ db/migrate/20181101194230_delete_scholarships_table.rb
@@ -1,6 +1,5 @@
 class DeleteScholarshipsTable < ActiveRecord::Migration[5.2]
   def change
-    remove_reference :scholarship_guides, :scholarships, foreign_key: true
-    drop_table :scholarships
+   
   end
 end
