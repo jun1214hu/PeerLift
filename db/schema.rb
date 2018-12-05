d4c334cf267d221878052d9728156c22cf6cde39
Jung Hur <junghur@college.harvard.edu>
2018-12-05 14:40:39 -0500

    added new columns to users for grade and school. Re-designed home page. Added color buttons and changed columns a bit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_232145) do
+ActiveRecord::Schema.define(version: 2018_12_05_180002) do
 
   create_table "file_uploads", force: :cascade do |t|
     t.integer "user_id"
@@ -109,6 +109,8 @@ ActiveRecord::Schema.define(version: 2018_12_04_232145) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "is_admin"
+    t.string "school"
+    t.string "grade"
   end
 
 end
